24e9e7680be1262c9afe8ed2354cbfc0faacb4a4
Jure Bevc <bevc.jure@gmail.com>
2021-05-04 14:23:28 +0200

    React native setup

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
